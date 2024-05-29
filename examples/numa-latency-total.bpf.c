a405adc920e28616f70c3aa534fc90b15862e8a3
ljs8860 <ljs8860@toss.im>
2024-05-29 18:54:47 +0900

    numa latency total 추가

---
+++ examples/numa-latency-total.bpf.c
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (c) 2020 Wenbo Zhang
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include "maps.bpf.h"
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 10240);
+	__type(key, u32);
+	__type(value, u64);
+} start SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 1);
+    __type(key, u32);
+    __type(value, u64);
+} numa_latency_total SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 1);
+    __type(key, u32);
+    __type(value, u64);
+} numa_move_total SEC(".maps");
+
+__u64 latency = 0;
+__u64 num = 0;
+
+static int __migrate_misplaced(void)
+{
+	u32 pid = bpf_get_current_pid_tgid();
+	u64 ts = bpf_ktime_get_ns();
+
+	bpf_map_update_elem(&start, &pid, &ts, 0);
+	return 0;
+}
+
+SEC("fentry/migrate_misplaced_page")
+int BPF_PROG(fentry_migrate_misplaced_page)
+{
+	return __migrate_misplaced();
+}
+
+SEC("fentry/migrate_misplaced_folio")
+int BPF_PROG(fentry_migrate_misplaced_folio)
+{
+	return __migrate_misplaced();
+}
+
+SEC("kprobe/migrate_misplaced_page")
+int BPF_PROG(kprobe_migrate_misplaced_page)
+{
+	return __migrate_misplaced();
+}
+
+SEC("kprobe/migrate_misplaced_folio")
+int BPF_PROG(kprobe_migrate_misplaced_folio)
+{
+	return __migrate_misplaced();
+}
+
+static int __migrate_misplaced_exit(void)
+{
+    u32 zero = 0;
+	u32 pid = bpf_get_current_pid_tgid();
+	u64 *tsp, ts = bpf_ktime_get_ns();
+	s64 delta;
+
+	tsp = bpf_map_lookup_elem(&start, &pid);
+	if (!tsp)
+		return 0;
+	delta = (s64)(ts - *tsp);
+	if (delta < 0)
+		goto cleanup;
+
+    increment_map(&numa_latency_total, &zero, delta / 1000000U);
+    increment_map(&numa_move_total, &zero, 1);
+
+cleanup:
+	bpf_map_delete_elem(&start, &pid);
+	return 0;
+}
+
+SEC("fexit/migrate_misplaced_page")
+int BPF_PROG(fexit_migrate_misplaced_page_exit)
+{
+	return __migrate_misplaced_exit();
+}
+
+SEC("fexit/migrate_misplaced_folio")
+int BPF_PROG(fexit_migrate_misplaced_folio_exit)
+{
+	return __migrate_misplaced_exit();
+}
+
+SEC("kretprobe/migrate_misplaced_page")
+int BPF_PROG(kretprobe_migrate_misplaced_page_exit)
+{
+	return __migrate_misplaced_exit();
+}
+
+SEC("kretprobe/migrate_misplaced_folio")
+int BPF_PROG(kretprobe_migrate_misplaced_folio_exit)
+{
+	return __migrate_misplaced_exit();
+}
+
+char LICENSE[] SEC("license") = "GPL";
